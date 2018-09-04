a6691d8965ab04221c678f292cac70d8229cc6ed
rangerix <kushalkanti1999@gmail.com>
2018-09-04 12:46:04 +0530

    iterate residue and atom wise

--- parse_pdb.py
+++ parse_pdb.py
@@ -5,7 +5,7 @@ from Bio import BiopythonWarning
 warnings.simplefilter('ignore', BiopythonWarning)                                   
 parser = PDBParser(PERMISSIVE=1)
 structure_id = "1fat"
-filename = "pdb1fat.ent"
+filename = "1fat.pdb"
 structure = parser.get_structure(structure_id, filename)
 
 
@@ -16,10 +16,28 @@ print(len(model.get_list()))
 
 print(structure.get_full_id())
 for chain in structure.get_chains():
-	print(chain)
 	i=0
 	for temp in chain.get_residues():
 		if PDB.is_aa(temp):
 			i+=1
-	print(i)			#length of each chain
+	print(chain," length : ",i)			#length of each chain
+
+
+print("for the chain : ",end='')
+print(chain)
+residues=chain.get_residues()	#print all residues of a chain
+for res in residues:
+	print("residue name : ",end='')
+	print(res.get_resname(),end='')
+	if PDB.is_aa(res):
+		print(" amino acid")
+	else:
+		print(" not amino acid")
+	print("atoms and their coordinates in the residue : ")
+	atoms=res.get_atom()
+	for atom in atoms:
+		print(atom.get_name(),end=' ')
+		print(atom.get_coord())
+	print('')
+     
 
